b3368c74a0ec3cc45c499d23355d625d21d36887
Oussama-Errahimi20032 <oussama.errahimi2003@gmail.com>
2024-01-16 23:33:42 +0100

    demo app

--- src/main/webapp/app/modules/home/home.tsx
+++ src/main/webapp/app/modules/home/home.tsx
@@ -1,9 +1,8 @@
 import './home.scss';
 
 import React from 'react';
-import { Link } from 'react-router-dom';
 import { Translate } from 'react-jhipster';
-import { Row, Col, Alert } from 'reactstrap';
+import { Col, Row } from 'reactstrap';
 
 import { useAppSelector } from 'app/config/store';
 import LoadingSpinner from 'app/shared/components/LoadingSpinner';
